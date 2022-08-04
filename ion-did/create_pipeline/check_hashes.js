21e602e37f1ef4b772121a0a9176902c440645c8
Sam Greenbury <sgreenbury@turing.ac.uk>
2022-08-04 10:15:01 +0100

    Initial create pipeline after adding proof v2

---
+++ ion-did/create_pipeline/check_hashes.js
@@ -0,0 +1,45 @@
+const ION = require('@decentralized-identity/ion-tools');
+const IONSDK = require('@decentralized-identity/ion-sdk');
+
+const fs = require('fs').promises
+// const fetch = require('cross-fetch');
+
+
+// TODO: checking whether we can hash the delta and get the same value as that recorded
+// in the request body.
+// 1. Expect uneditted to be same. IT IS!
+// 2. Expect any edit to diff. IT IS!
+// Hope that if edit to diff, and update the deltaHash in the request body, it successfully resolves on ION.
+
+const main = async () => {
+    // Load request body from `ion operation create`
+    // const requestBody = JSON.parse(await fs.readFile("request-body-EiAvZ_Dl0jHYI3zroklAlnZx9BsPKvipXxJf63ko_gTORQ.json"))
+    // const requestBody = JSON.parse(await fs.readFile("request-body-editted-EiAvZ_Dl0jHYI3zroklAlnZx9BsPKvipXxJf63ko_gTORQ.json"))
+    // const requestBody = JSON.parse(await fs.readFile("anchorRequestBody_290722.json"))
+    const requestBody = JSON.parse(await fs.readFile("anchorRequestBody_030822.json"))
+    // console.log(JSON.stringify(requestBody))
+
+
+    const delta = requestBody.delta
+    
+    // Get the delta
+    console.log(JSON.stringify(delta))
+
+    // ION.SDK.IonSdkConfig.hashAlgorithmInMultihashCode
+    // ION.SDK.IonSdkConfig
+    // console.log("")
+    // Get the hash algo
+    const hashAlgorithmInMultihashCode = IONSDK.IonSdkConfig.hashAlgorithmInMultihashCode;
+    
+    // IONSDK.IonRequest.
+    // const hashAlgorithmInMultihashCode = 18;
+    // IONSDK
+    console.log(ION.SDK.IonSdkConfig.hashAlgorithmInMultihashCode);
+
+    // Hash the delta
+    const deltaHash = IONSDK.MultiHash.canonicalizeThenHashThenEncode(delta, hashAlgorithmInMultihashCode);
+
+    console.log(deltaHash);
+}
+
+main()
---
+++ ion-did/create_pipeline/make_did_step3.js
@@ -0,0 +1,73 @@
+const ION = require('@decentralized-identity/ion-tools')
+const IONSDK = require('@decentralized-identity/ion-sdk');
+const fs = require('fs').promises
+const fetch = require('cross-fetch');
+
+const main = async () => {
+    
+    // Step 1: load keys
+    const privateKey = JSON.parse(await fs.readFile('privateKey.json'))
+    const myData = 'This message is signed and cannot be tampered with'
+    const signature = await ION.signJws({
+        payload: myData,
+        privateJwk: privateKey
+    });
+    console.log("Signed JWS:", signature)
+    
+
+    // Testing
+    const randomKeyPair = await ION.generateKeyPair('secp256k1')
+    let verifiedJws = await ION.verifyJws({
+        jws: signature,
+        publicJwk: randomKeyPair.publicJwk
+    })
+    console.log("Verify with random new key:", verifiedJws)
+    
+    const publicKey = JSON.parse(await fs.readFile('publicKey.json'))
+    verifiedJws = await ION.verifyJws({
+        jws: signature,
+        publicJwk: publicKey
+    })
+    console.log("Verify with my public key:", verifiedJws)
+
+
+    //  Make object to store the keys
+    const authnKeys = {
+        privateJwk: privateKey,
+        publicJwk: publicKey
+    };
+
+    // Load DID Request Body
+    const didRequestBody = JSON.parse(await fs.readFile('anchorRequestBody_prototype_030822.json'))
+
+    //  Hashing the delta. 
+    // We have edited the did request body and need to rehash. 
+    const delta = didRequestBody.delta.
+
+    const hashAlgorithmInMultihashCode = IONSDK.IonSdkConfig.hashAlgorithmInMultihashCode;
+
+    const deltaHash = IONSDK.MultiHash.canonicalizeThenHashThenEncode(delta, hashAlgorithmInMultihashCode);
+
+    console.log(deltaHash);
+
+    // const didUri = await did.getURI('short');
+
+    // Sign new DID
+    const signature_did = await ION.signJws({
+        payload: deltaHash,
+        privateJwk: privateKey
+    });
+    console.log("Signed JWS:", signature_did)
+
+    // Testing
+
+    let verifiedSignedHash = await ION.verifyJws({
+        jws: signature_did,
+        publicJwk: publicKey
+    })
+    console.log("Verify Signed Hash:", verifiedSignedHash)
+    
+
+}
+
+main()
