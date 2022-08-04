a9cc481afa20034e653d73d01efcff546736b7e3
Sam Greenbury <sgreenbury@turing.ac.uk>
2022-08-04 12:00:39 +0100

    Refine: sign public key, recompute deltaHash, modify request

--- ion-did/create_pipeline/make_did_step2.js
+++ ion-did/create_pipeline/make_did_step2.js
@@ -1,35 +1,15 @@
 const ION = require('@decentralized-identity/ion-tools')
 const IONSDK = require('@decentralized-identity/ion-sdk');
+const jwt_decode = require('jwt-decode');
 const fs = require('fs').promises
 const fetch = require('cross-fetch');
+const { AnchorRequest } = require('@decentralized-identity/ion-tools');
 
 const main = async () => {
-    
-    // Step 1: load keys
-    const privateKey = JSON.parse(await fs.readFile('privateKey.json'))
-    const myData = 'This message is signed and cannot be tampered with'
-    const signature = await ION.signJws({
-        payload: myData,
-        privateJwk: privateKey
-    });
-    console.log("Signed JWS:", signature)
-    
-
-    // Testing
-    const randomKeyPair = await ION.generateKeyPair('secp256k1')
-    let verifiedJws = await ION.verifyJws({
-        jws: signature,
-        publicJwk: randomKeyPair.publicJwk
-    })
-    console.log("Verify with random new key:", verifiedJws)
-    
+    // Load keys and store in authnKeys for signing of DID
+    const privateKey = JSON.parse(await fs.readFile('privateKey.json'));
     const publicKey = JSON.parse(await fs.readFile('publicKey.json'))
-    verifiedJws = await ION.verifyJws({
-        jws: signature,
-        publicJwk: publicKey
-    })
-    console.log("Verify with my public key:", verifiedJws)
-
+    const controlleruDID = "did:ion:test:upstream_DID_that_is_the_controller"
 
     //  Make object to store the keys
     const authnKeys = {
@@ -38,36 +18,65 @@ const main = async () => {
     };
 
     // Load DID Request Body
-    const didRequestBody = JSON.parse(await fs.readFile('anchorRequestBody_030822.json'))
+    const didRequestBody = JSON.parse(await fs.readFile('anchorRequestBody.json'))
 
-    //  Hashing the delta. 
-    // We have edited the did request body and need to rehash. 
-    const delta = didRequestBody.delta
+    // Add uDID controller to request
+    didRequestBody.delta["patches"][0]["document"]["services"][0]["serviceEndpoint"]["signer"] = {"uDID": controlleruDID}
 
+    // Hashing the delta, if unedited should be the same as that generated 
+    // const delta = didRequestBody.delta
+    // const hashAlgorithmInMultihashCode = IONSDK.IonSdkConfig.hashAlgorithmInMultihashCode;
+    // const deltaHash = IONSDK.MultiHash.canonicalizeThenHashThenEncode(delta, hashAlgorithmInMultihashCode);
+    // console.log("Recomputed deltaHash:", deltaHash);
+    // console.log("Same deltHash as generated request:", deltaHash == didRequestBody.suffixData.deltaHash);
+
+    // Just sign the publicJWK with uDID controller keys for simplicity
+    // TODO: consider whether more information than just the keys needs to be signed
+    // Of note, the DID is derived from the deltaHash and should be derivable from the
+    // resolved DID which includes the signed public key, so this is verfiable and only
+    // derivable with the same signed public key
+    
+    // Get JWK of public key
+    const requestJWK = didRequestBody.delta["patches"][0]["document"]["publicKeys"][0]["publicKeyJwk"];
+    // Get hash algorithm and hash JWK
     const hashAlgorithmInMultihashCode = IONSDK.IonSdkConfig.hashAlgorithmInMultihashCode;
+    const requestJWKHash = IONSDK.MultiHash.canonicalizeThenHashThenEncode(requestJWK, hashAlgorithmInMultihashCode);
 
-    const deltaHash = IONSDK.MultiHash.canonicalizeThenHashThenEncode(delta, hashAlgorithmInMultihashCode);
-
-    console.log(deltaHash);
-
-    // const didUri = await did.getURI('short');
-
-    // Sign new DID
-    const signature_did = await ION.signJws({
-        payload: deltaHash,
+    // Sign JWK as JWS
+    const signedJWKHash = await ION.signJws({
+        payload: requestJWKHash,
         privateJwk: privateKey
     });
-    console.log("Signed JWS:", signature_did)
+    // Print JWS ignature
+    console.log("Signed JWS of JWKHash to be anchored:", signedJWKHash)
 
-    // Testing
-
+    // Optional: Verify signature
     let verifiedSignedHash = await ION.verifyJws({
-        jws: signature_did,
+        jws: signedJWKHash,
         publicJwk: publicKey
     })
     console.log("Verify Signed Hash:", verifiedSignedHash)
     
+    // Decode JWT to confirm hash is same
+    var decodedJWKHash = jwt_decode(signedJWKHash);
+    console.log("Decoded signed payload:", decodedJWKHash);
+    console.log("Decoded same as signed:", decodedJWKHash == requestJWKHash);
 
+    // Add the signed hash of the JWK to the request
+    didRequestBody.delta["patches"][0]["document"]["services"][0]["serviceEndpoint"]["signer"]["proof"] = signedJWKHash;
+
+    // Recompute and replace the deltaHash
+    const newDeltaHash = IONSDK.MultiHash.canonicalizeThenHashThenEncode(didRequestBody.delta, hashAlgorithmInMultihashCode);
+    didRequestBody["suffixData"]["deltaHash"] = newDeltaHash;
+
+    // Print final request body
+    console.log(JSON.stringify(didRequestBody, null, 2))
+
+    // Write request body
+    await fs.writeFile(
+       'anchorRequestBody_with_proof.json', 
+        JSON.stringify(didRequestBody)
+    )
 }
 
 main()
