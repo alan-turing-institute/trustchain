24f917af06e4201aab48c295ac9bd3871d8ef3f4
Sam Greenbury <sgreenbury@turing.ac.uk>
2022-10-28 17:21:40 +0100

    Max depth 1 for publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,7 +9,7 @@
 # Returns:
 #   OK / Bad Request
 
-for file in $(find "$HOME/.trustchain/operations" -name "*.json"); do
+for file in $(find "$HOME/.trustchain/operations" -name "*.json" -depth 1); do
     echo $file
     curl --tr-encoding -X POST -v -# -o "$file.out.json" -T $file -H "Content-Type: application/json; charset=utf-8" http://localhost:3000/operations
 done
