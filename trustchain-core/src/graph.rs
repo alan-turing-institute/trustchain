08089f4ff4f7d9e797df56cc451200134730cdb9
Sam Greenbury <sgreenbury@turing.ac.uk>
2022-11-03 13:22:35 +0000

    Refactor TrustchainGraph to read chains and label with PrettyDID

--- trustchain-core/src/graph.rs
+++ trustchain-core/src/graph.rs
@@ -1,7 +1,10 @@
+use crate::chain::{Chain, DIDChain, PrettyDID};
 use petgraph::adj::NodeIndex;
 use petgraph::dot::{Config, Dot};
-use petgraph::graph::DiGraph;
+use petgraph::graph::{DiGraph, Node};
+use petgraph::prelude::DiGraphMap;
 use ssi::did::Document;
+use std::collections::{HashMap, HashSet};
 use std::fmt::Display;
 use thiserror::Error;
 
@@ -18,33 +21,62 @@ pub enum GraphError {
 #[derive(Debug)]
 struct TrustchainGraph {
     // TODO: check this is correct type spec
-    graph: DiGraph<String, ()>,
+    graph: DiGraph<String, String>,
 }
 
-/// Read forest from a vector of vectors (list of trees) and return a DiGraph.
+/// Read chains from a vector and return a DiGraph.
 /// See: https://docs.rs/petgraph/latest/petgraph/graph/struct.Graph.html
-fn read_forest(forest: &Vec<Vec<Document>>) -> DiGraph<String, ()> {
-    let mut graph = DiGraph::new();
-    let mut edges = Vec::new();
-    for tree in forest {
-        for i in 1..tree.len() {
-            // TODO: check if node already present? Might need new struct (e.g. HashMap)
-            // to store nodes as encountered and only add once.
-            // TODO: consider whether to use a formatted string with more than DID
-            // as graph label (e.g. truncated DID, service info, key info, etc)
-            let ns = graph.add_node(tree[i - 1].id.clone());
-            let nt = graph.add_node(tree[i].id.clone());
-            edges.push((ns, nt));
+fn read_chains(chains: &Vec<DIDChain>) -> DiGraph<String, String> {
+    let mut nodes = HashMap::<String, petgraph::prelude::NodeIndex>::new();
+    let mut graph = DiGraph::<String, String>::new();
+    for chain in chains {
+        let mut did = chain.root().to_owned();
+        let mut level = 0;
+        while let Some(ddid) = chain.downstream(&did) {
+            // Add target
+            let nt = match nodes.get(ddid) {
+                Some(&v) => v,
+                None => {
+                    let pretty_ddid =
+                        PrettyDID::from((&chain.data(ddid).unwrap().0, level + 1)).to_node_string();
+                    let nt = graph.add_node(pretty_ddid);
+                    nodes.insert(ddid.to_owned(), nt);
+                    nt
+                }
+            };
+            // Add source
+            let ns = match nodes.get(&did) {
+                Some(&v) => v,
+                None => {
+                    let pretty_did =
+                        PrettyDID::from((&chain.data(&did).unwrap().0, level)).to_node_string();
+                    let ns = graph.add_node(pretty_did);
+                    nodes.insert(did.to_owned(), ns);
+                    ns
+                }
+            };
+            if !graph.contains_edge(ns, nt) {
+                graph.extend_with_edges([(ns, nt, "".to_string())]);
+            }
+
+            // Update udid and did
+            did = ddid.to_owned();
+            level += 1;
         }
     }
-    graph.extend_with_edges(&edges);
     graph
+}
+
+impl From<Vec<DIDChain>> for TrustchainGraph {
+    fn from(chains: Vec<DIDChain>) -> Self {
+        TrustchainGraph::new(&chains).unwrap()
+    }
 }
 
 impl TrustchainGraph {
     /// Makes a new TrustchainGraph instance.
-    fn new(forest: &Vec<Vec<Document>>) -> Result<Self, GraphError> {
-        let graph = read_forest(&forest);
+    fn new(chains: &Vec<DIDChain>) -> Result<Self, GraphError> {
+        let graph = read_chains(chains);
         Ok(Self { graph })
     }
 
@@ -71,27 +103,36 @@ impl Display for TrustchainGraph {
 mod tests {
     use super::*;
     use crate::data::{
-        TEST_SIDETREE_DOCUMENT, TEST_SIDETREE_DOCUMENT_METADATA,
-        TEST_SIDETREE_DOCUMENT_MULTIPLE_PROOF, TEST_SIDETREE_DOCUMENT_SERVICE_AND_PROOF,
-        TEST_SIDETREE_DOCUMENT_SERVICE_NOT_PROOF, TEST_SIDETREE_DOCUMENT_WITH_CONTROLLER,
-        TEST_TRUSTCHAIN_DOCUMENT, TEST_TRUSTCHAIN_DOCUMENT_METADATA,
+        TEST_ROOT_DOCUMENT, TEST_ROOT_DOCUMENT_METADATA, TEST_ROOT_PLUS_1_DOCUMENT,
+        TEST_ROOT_PLUS_1_DOCUMENT_METADATA, TEST_ROOT_PLUS_2_DOCUMENT,
+        TEST_ROOT_PLUS_2_DOCUMENT_METADATA, TEST_SIDETREE_DOCUMENT,
+        TEST_SIDETREE_DOCUMENT_METADATA, TEST_SIDETREE_DOCUMENT_MULTIPLE_PROOF,
+        TEST_SIDETREE_DOCUMENT_SERVICE_AND_PROOF, TEST_SIDETREE_DOCUMENT_SERVICE_NOT_PROOF,
+        TEST_SIDETREE_DOCUMENT_WITH_CONTROLLER, TEST_TRUSTCHAIN_DOCUMENT,
+        TEST_TRUSTCHAIN_DOCUMENT_METADATA,
     };
+    // TODO: temporary public tests for test chain before test chain fixture available
+    use crate::chain::tests::test_chain;
     use crate::utils::canonicalize;
 
     #[test]
-    fn read_forest() {
-        let doc1: Document = serde_json::from_str(TEST_TRUSTCHAIN_DOCUMENT).unwrap();
-        let doc2: Document = serde_json::from_str(TEST_TRUSTCHAIN_DOCUMENT).unwrap();
-        let mut forest = Vec::new();
-        forest.push(vec![doc1, doc2]);
-        let graph = TrustchainGraph::new(&forest).unwrap();
+    fn read_chains() {
+        // let doc1: Document = serde_json::from_str(TEST_TRUSTCHAIN_DOCUMENT).unwrap();
+        // let doc2: Document = serde_json::from_str(TEST_TRUSTCHAIN_DOCUMENT).unwrap();
+        // let mut forest = Vec::new();
+        // forest.push(vec![doc1, doc2]);
+        let chains = vec![test_chain().unwrap(), test_chain().unwrap()];
+        let graph = TrustchainGraph::new(&chains).unwrap();
 
         // Output the tree to `graphviz` `DOT` format
         let dot = Dot::with_config(
             &graph.graph,
             &[Config::GraphContentOnly, Config::EdgeNoLabel],
         );
-        println!("{:?}", dot);
+        // let g: Graph = parse(
+        println!("{}", "DiGraph {".to_owned());
+        println!("{}", dot);
+        println!("{}", "}".to_owned());
     }
     // #[test]
     // fn invalid_not_a_tree() {
