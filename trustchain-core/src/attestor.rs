5808699a07b00e1e871a083ba82976618ea64165
Sam Greenbury <sgreenbury@turing.ac.uk>
2022-11-21 15:50:33 +0000

    Fix and refine comments, remove usused tests submodule

--- trustchain-core/src/attestor.rs
+++ trustchain-core/src/attestor.rs
@@ -8,16 +8,16 @@ pub enum AttestorError {
     /// No trustchain subject.
     #[error("DID: {0} as Trustchain subject does not exist.")]
     NoTrustchainSubject(String),
-    /// No signing key of passed ID.
+    /// No signing key available.
     #[error("No signing key available for DID {0}.")]
     NoSigningKey(String),
-    /// No signing key of passed ID.
+    /// No signing key with specified ID.
     #[error("DID: {0} with signing key idx {1} does not exist.")]
     NoSigningKeyWithId(String, String),
     /// Invalid document for attestation.
     #[error("Document with DID {0} has invalid parameters.")]
     InvalidDocumentParameters(String),
-    /// Invalid document for attestation.
+    /// Failed to sign DID document.
     #[error("Signing error for Document with DID {0}: {1}.")]
     SigningError(String, String),
 }
