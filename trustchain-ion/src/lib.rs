3e10b0edb3524bf2ecda97e8b990aeb610f31741
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-01-09 16:16:31 +0000

    Move test str to create module tests

--- trustchain-ion/src/lib.rs
+++ trustchain-ion/src/lib.rs
@@ -25,36 +25,3 @@ pub enum TrustchainIONError {
     #[error("Incorrect key type is provided.")]
     IncorrectKeyType,
 }
-
-// TODO: move to fixtures for trustchain-ion
-const TEST_DOC_STATE: &str = r##"{
-    "publicKeys": [
-       {
-          "id": "Mz94EfSCueClM5qv62SXxtLWRj4Ti7rR2wLWmW37aCs",
-          "type": "JsonWebSignature2020",
-          "publicKeyJwk": {
-          "crv": "secp256k1",
-          "kty": "EC",
-          "x": "7VKmPezI_VEnMjOPfAeUnpQxhS1sLjAKfd0s7xrmx9A",
-          "y": "gWZ5Bo197eZuMh3Se-3rqWCQjZWbuDpOYAaw8yC-yaQ"
-          },
-          "purposes": [
-          "assertionMethod",
-          "authentication",
-          "keyAgreement",
-          "capabilityInvocation",
-          "capabilityDelegation"
-          ]
-       }
-    ],
-    "services": [
-       {
-          "id": "trustchain-controller-proof",
-          "type": "TrustchainProofService",
-          "serviceEndpoint": {
-          "controller": "did:ion:test:EiA8yZGuDKbcnmPRs9ywaCsoE2FT9HMuyD9WmOiQasxBBg",
-          "proofValue": "dummy_string"
-          }
-       }
-    ]
- }"##;
