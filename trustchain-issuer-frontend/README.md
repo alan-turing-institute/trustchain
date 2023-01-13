65d282adc884988ccc06d96b70810122e837ae71
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-01-13 14:01:34 +0000

    WIP: example yew frontend

---
+++ trustchain-issuer-frontend/README.md
@@ -0,0 +1,11 @@
+# Frontend notes
+
+[Needs](https://stackoverflow.com/questions/72146492/unable-to-execute-trunk-serve):
+```
+cargo install --locked wasm-bindgen-cli
+```
+
+Run with:
+```
+trunk serve
+```
---
+++ trustchain-issuer-frontend/src/app.rs
@@ -0,0 +1,20 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+pub fn app() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
+    }
+}
