ffd24400d58b2ced92f3c2b95dbb72ddcfc3e882
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-01-19 11:57:15 +0000

    Pass credential through stdin with flexible subject and credential id

--- trustchain-issuer-backend/src/main.rs
+++ trustchain-issuer-backend/src/main.rs
@@ -2,14 +2,21 @@ use actix_web::Result as ActixResult;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use base64::engine::general_purpose;
 use base64::write::EncoderWriter;
-use execute::Execute;
-use image::Luma;
 use image::{DynamicImage, ImageOutputFormat};
+use image::{EncodableLayout, Luma};
 use qrcode::QrCode;
 use serde::{Deserialize, Serialize};
 use serde_json::{to_string_pretty, Map, Value};
+use ssi::one_or_many::OneOrMany;
+use ssi::vc::Credential;
+use std::io::Write;
 use std::process::{Command, Stdio};
 use uuid::Uuid;
+
+/// Server localhost address
+const HOST: &str = "http://127.0.0.1:8081";
+/// Android server localhost address
+// const HOST: &str = "http://10.0.2.2:8081";
 
 // Process sketch:
 // 1. User visits "credentials/" page and is displayed a QR code of a URI (with UUID) to send GET request to
@@ -37,11 +44,11 @@ pub struct MyParams {
 /// Simple handle POST request (see [examples](https://github.com/actix/examples/blob/master/forms/form/src/main.rs))
 async fn handle_issuer_post_start(_params: web::Form<MyParams>) -> ActixResult<HttpResponse> {
     // Generate a UUID
-    let id = Uuid::new_v4().to_string().replace("-", "");
+    // let id = Uuid::new_v4().to_string().replace("-", "");
+    let id = Uuid::new_v4().to_string();
 
     // Generate a QR code for server address and combination of name and UUID
-    let address_str = format!("http://127.0.0.1:8081/vc/{id}");
-    // let address_str = format!("http://10.0.2.2:8081/vc/{id}");
+    let address_str = format!("{HOST}/vc/{id}");
     let code = QrCode::new(address_str.as_bytes()).unwrap();
 
     // Render the bits into an image.
@@ -112,31 +119,56 @@ async fn post_request(info: web::Json<VcInfo>, id: web::Path<String>) -> impl Re
 }
 
 fn handle_post_vc(subject_id: &str, credential_id: &str) -> String {
-    // TODO: generate_vc should take the subject_id and insert that DID
-    // into the subject id field when creating the credential. But for now
-    // this is hard-coded in the sample credential named:
-    // credential_for_did-tz-tz1hne1ao44vqbVTo4ttWjokieJrdEZW9b8C.jsonld
     generate_vc(false, Some(subject_id), credential_id)
 }
 
+/// Converts a credential into an offer
+
 /// Generates a VC (prototype uses const DID and const credential file)
 fn generate_vc(is_offer: bool, subject_id: Option<&str>, credential_id: &str) -> String {
-    // Note: we ignore the subject_id for now - just testing with hard-code DID fields.
-    let home = std::env::var("HOME").unwrap();
-    // TODO: read credential here, update with UUID:
-    //   - Pass into command str?
-    //   - Read credential string as bytes from stdin
-    let command_str = format!("trustchain-cli vc attest --credential_file {}/.trustchain/credentials/credential_for_did-tz-tz1hne1ao44vqbVTo4ttWjokieJrdEZW9b8C.jsonld --did did:ion:test:EiBYdto2LQd_uAj_EXEoxP_KbLmZzwe1E-vXp8ZsMv1Gpg", home);
-    let mut iter = command_str.split(" ");
-    let mut command = Command::new(iter.next().unwrap());
-    for s in iter {
-        println!("{}", s);
-        command.arg(s);
-    }
+    let command_str = format!("trustchain-cli vc attest --did did:ion:test:EiBYdto2LQd_uAj_EXEoxP_KbLmZzwe1E-vXp8ZsMv1Gpg");
+    let mut command = command_str.split(" ").skip(1).fold(
+        Command::new(command_str.split_once(' ').unwrap().0),
+        |mut cmd, s| {
+            cmd.arg(s);
+            cmd
+        },
+    );
     println!("{:?}", command);
-    command.stdout(Stdio::piped()).stderr(Stdio::piped());
+    command
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .stdin(Stdio::piped());
 
-    let output = command.execute_output().unwrap();
+    // Read credential as string
+    let home = std::env::var("HOME").unwrap();
+    let file_str = format!("{home}/.trustchain/credentials/credential_template.jsonld");
+    let mut credential: Credential =
+        serde_json::from_reader(std::fs::read(file_str).unwrap().as_bytes()).unwrap();
+
+    // Add passed credential_id
+    credential.id = Some(ssi::vc::URI::String(format!(
+        "urn:uuid:{}",
+        credential_id.to_string()
+    )));
+
+    // Add subject_id if not none
+    if let Some(subject_id_str) = subject_id {
+        if let OneOrMany::One(ref mut subject) = credential.credential_subject {
+            subject.id = Some(ssi::vc::URI::String(subject_id_str.to_string()));
+        }
+    }
+    // Use stdin to pass credential string
+    let mut child = command.spawn().expect("Failed to spawn child process");
+    let mut stdin = child.stdin.take().expect("Failed to open stdin");
+    std::thread::spawn(move || {
+        stdin
+            .write_all(serde_json::to_string(&credential).unwrap().as_bytes())
+            .expect("Failed to write to stdin");
+    });
+    // Execute
+    let output = child.wait_with_output().expect("Failed to read stdout");
+
     println!("{}", String::from_utf8(output.clone().stdout).unwrap());
     println!("{}", String::from_utf8(output.clone().stderr).unwrap());
 
@@ -148,38 +180,40 @@ fn generate_vc(is_offer: bool, subject_id: Option<&str>, credential_id: &str) ->
     //   "credentialPreview":{<CREDENTIAL_PREVIEW>},
     //   "expires":"2022-12-20T14:19:49Z"
     // }
-    let mut vc: Map<String, Value> = serde_json::from_str(vc_string.as_str()).unwrap();
 
     if !is_offer {
-        let vc_string = serde_json::to_string_pretty(&vc).unwrap();
+        // let vc_string = serde_json::to_string_pretty(&vc).unwrap();
         println!("CREDENTIAL:\n{}", vc_string);
-        return vc_string;
+        vc_string
+    } else {
+        // If offer on
+        let mut vc: Map<String, Value> = serde_json::from_str(vc_string.as_str()).unwrap();
+
+        // Remove proof
+        vc.remove("proof");
+
+        // Make our credential offer
+        let mut offer: Map<String, Value> = Map::new();
+
+        // Type: credential offer
+        offer.insert(
+            "type".to_string(),
+            Value::String("CredentialOffer".to_string()),
+        );
+
+        // Preview with the VC excluding proof
+        offer.insert("credentialPreview".to_string(), Value::Object(vc));
+
+        // Insert expiry time for using offer within next hour
+        let expiry_time = chrono::offset::Utc::now() + chrono::Duration::minutes(60);
+        offer.insert(
+            "expires".to_string(),
+            Value::String(expiry_time.format("%Y-%m-%dT%H:%M:%S%.3fZ").to_string()),
+        );
+        println!("OFFER:");
+        println!("{}", to_string_pretty(&offer).unwrap());
+        String::from_utf8(serde_json::to_string(&offer).unwrap().into_bytes()).unwrap()
     }
-
-    // Remove proof
-    vc.remove("proof");
-
-    // Make our credential offer
-    let mut offer: Map<String, Value> = Map::new();
-
-    // Type: credential offer
-    offer.insert(
-        "type".to_string(),
-        Value::String("CredentialOffer".to_string()),
-    );
-
-    // Our VC
-    offer.insert("credentialPreview".to_string(), Value::Object(vc));
-
-    // 2022-12-20T14:19:49Z
-    // time
-    offer.insert(
-        "expires".to_string(),
-        Value::String("2022-12-21T14:19:49Z".to_string()),
-    );
-    println!("OFFER:");
-    println!("{}", to_string_pretty(&offer).unwrap());
-    String::from_utf8(serde_json::to_string(&offer).unwrap().into_bytes()).unwrap()
 }
 
 #[actix_web::main] // or #[tokio::main]
