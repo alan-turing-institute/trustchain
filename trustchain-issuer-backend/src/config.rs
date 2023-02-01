f3cba688e20ddb853410a8e03c60e79a508d0ff3
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-02-01 08:26:01 +0000

    Better types in config

--- trustchain-issuer-backend/src/config.rs
+++ trustchain-issuer-backend/src/config.rs
@@ -1,12 +1,13 @@
 use clap::StructOpt;
+use std::net::IpAddr;
 
 /// Config for server.
 #[derive(StructOpt, Debug, Clone)]
 pub struct ServerConfig {
     /// Hostname for server
     #[clap(env, short = 's', long)]
-    pub host: String,
+    pub host: IpAddr,
     /// Port for server
     #[clap(env, short = 'p', long)]
-    pub port: String,
+    pub port: u16,
 }
