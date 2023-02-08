c801206936028f7030600dddd4e8af3496dd0773
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-02-08 09:31:32 +0000

    Initial FFI with flutter rust bridge

--- trustchain-ion/src/lib.rs
+++ trustchain-ion/src/lib.rs
@@ -1,5 +1,7 @@
+pub mod api;
 pub mod attest;
 pub mod attestor;
+mod bridge_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 pub mod controller;
 pub mod verifier;
 
