2687216c61c770a7db6e94ee306645ce2ff8ca40
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-03-16 09:34:33 +0000

    Add resolver error with resolution metadata

--- trustchain-core/src/resolver.rs
+++ trustchain-core/src/resolver.rs
@@ -13,7 +13,7 @@ use tokio::runtime::Runtime;
 use crate::TRUSTCHAIN_PROOF_SERVICE_ID_VALUE;
 
 /// An error relating to Trustchain resolution.
-#[derive(Error, Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Error, Debug)]
 pub enum ResolverError {
     /// Controller is already present in DID document.
     #[error("Controller is already present in DID document.")]
@@ -36,6 +36,9 @@ pub enum ResolverError {
     /// DID is not found.
     #[error("DID: {0} is not found.")]
     DIDNotFound(String),
+    /// General resolver error with resolution metadata.
+    #[error("Resolver error with resolution metadata.")]
+    FailureWithMetadata(ResolutionMetadata),
 }
 
 /// Type for resolver result.
@@ -502,11 +505,12 @@ mod tests {
 
         // Attempt to add the controller.
         let result = resolver.add_controller(did_doc, &controller_did);
-        let expected: Result<Document, ResolverError> =
-            Err(ResolverError::ControllerAlreadyPresent);
 
         // Confirm error.
-        assert_eq!(result, expected);
+        assert!(matches!(
+            result,
+            Err(ResolverError::ControllerAlreadyPresent)
+        ));
     }
 
     #[test]
@@ -597,10 +601,10 @@ mod tests {
         let result = resolver.get_proof_service(&did_doc);
 
         // Expect an error due to the presence of multiple proof services.
-        let expected: Result<&Service, ResolverError> =
-            Err(ResolverError::MultipleTrustchainProofService);
-
-        assert_eq!(result, expected);
+        assert!(matches!(
+            result,
+            Err(ResolverError::MultipleTrustchainProofService)
+        ));
     }
 
     #[test]
@@ -619,11 +623,11 @@ mod tests {
 
         let result = resolver.get_proof_service(&did_doc);
 
-        // Expect an error due to the absence of any proof services.
-        let expected: Result<&Service, ResolverError> =
-            Err(ResolverError::NoTrustchainProofService);
-
-        assert_eq!(result, expected);
+        // // Expect an error due to the absence of any proof services.
+        assert!(matches!(
+            result,
+            Err(ResolverError::NoTrustchainProofService)
+        ));
     }
 
     #[test]
@@ -643,10 +647,10 @@ mod tests {
         let result = resolver.get_proof_service(&did_doc);
 
         // Expect an error due to the absence of any proof services.
-        let expected: Result<&Service, ResolverError> =
-            Err(ResolverError::NoTrustchainProofService);
-
-        assert_eq!(result, expected);
+        assert!(matches!(
+            result,
+            Err(ResolverError::NoTrustchainProofService)
+        ));
     }
 
     #[test]
@@ -849,9 +853,9 @@ mod tests {
         );
 
         // Check for the correct error.
-        match output {
-            Err(e) => assert_eq!(e, ResolverError::MultipleTrustchainProofService),
-            _ => panic!(),
-        }
+        assert!(matches!(
+            output,
+            Err(ResolverError::MultipleTrustchainProofService)
+        ));
     }
 }
