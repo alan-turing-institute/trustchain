ff81619dc9a0f3d96422b30311ca211f07aa02cd
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-03-30 09:12:22 +0100

    Move methods of trait prototype into gui module

--- trustchain-ffi/src/gui.rs
+++ trustchain-ffi/src/gui.rs
@@ -1,8 +1,8 @@
 use serde_json::to_string_pretty;
+use trustchain_cli::TrustchainCLI;
 use trustchain_core::chain::DIDChain;
 use trustchain_core::verifier::Verifier;
 use trustchain_ion::{get_ion_resolver, verifier::IONVerifier};
-// use trustchain_core::{ROOT_EVENT_TIME, ROOT_EVENT_TIME_2378493};
 
 /// Example greet function.
 pub fn greet() -> String {
@@ -10,7 +10,7 @@ pub fn greet() -> String {
 }
 
 /// Example resolve interface.
-pub fn resolve(did: String) -> String {
+pub fn resolve_prototype(did: String) -> String {
     // Trustchain Resolver with android localhost
     let resolver = get_ion_resolver("http://127.0.0.1:3000/");
     // Result metadata, Document, Document metadata
@@ -20,10 +20,53 @@ pub fn resolve(did: String) -> String {
 
 //"did:ion:test:EiCzekHARUPkqf0NRsQ6kfpcnEbwtpdTIgadTYWaggx8Rg"
 // ROOT_EVENT_TIME_2378493
-pub fn verify(did: String, root_timestamp: u32) -> DIDChain {
+pub fn verify_prototype(did: String, root_timestamp: u32) -> DIDChain {
     // Construct a Trustchain Resolver from a Sidetree (ION) DIDMethod.
     let resolver = get_ion_resolver("http://localhost:3000/");
     let verifier = IONVerifier::new(resolver);
 
     verifier.verify(&did, root_timestamp).unwrap()
+}
+
+// TODO: implement the below functions that will be used as FFI on desktop GUI. Aim to implement the
+// functions to that they each call a TrustchainCLI method.
+//
+// NOTE: There is currently an [open pull request](https://github.com/fzyzcjy/flutter_rust_bridge/pull/582)
+// for support of the rust Result type which will add the functionality of returning custom error
+// types rather than only a custom error message (&str).
+
+/// Creates a controlled DID from a passed document state, writing the associated create operation to file in the operations path.
+fn create(document_state: Option<String>, verbose: bool) -> anyhow::Result<()> {
+    todo!()
+}
+
+/// An uDID attests to a dDID, writing the associated update operation to file in the operations path.
+fn attest(did: String, controlled_did: String, verbose: bool) -> anyhow::Result<()> {
+    todo!()
+}
+/// Resolves a given DID using a resolver available at localhost:3000
+fn resolve(did: String, verbose: bool) -> anyhow::Result<()> {
+    todo!()
+}
+
+/// TODO: the below have no CLI implementation currently but are planned
+/// Verifies a given DID using a resolver available at localhost:3000, returning a result.
+fn verify(did: String, verbose: bool) -> anyhow::Result<DIDChain> {
+    todo!()
+}
+/// Generates an update operation and writes to operations path.
+fn update(did: String, controlled_did: String, verbose: bool) -> anyhow::Result<()> {
+    todo!()
+}
+/// Generates a recover operation and writes to operations path.
+fn recover(did: String, verbose: bool) -> anyhow::Result<()> {
+    todo!()
+}
+/// Generates a deactivate operation and writes to operations path.
+fn deactivate(did: String, verbose: bool) -> anyhow::Result<()> {
+    todo!()
+}
+/// Publishes operations within the operations path (queue).
+fn publish(did: String, verbose: bool) -> anyhow::Result<()> {
+    todo!()
 }
