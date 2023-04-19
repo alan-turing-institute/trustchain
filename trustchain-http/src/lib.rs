e851c35818423e64d7c81bfdcd3fb1788531188c
Luke Hare <lhare@turing.ac.uk>
2023-04-19 11:04:45 +0100

    Add TODO

--- trustchain-http/src/lib.rs
+++ trustchain-http/src/lib.rs
@@ -12,3 +12,5 @@ pub const ISSUER_DID: &str = "did:ion:test:EiBYdto2LQd_uAj_EXEoxP_KbLmZzwe1E-vXp
 
 /// Example VP request used by demo.spruceid.com
 pub const EXAMPLE_VP_REQUEST: &str = r#"{ "type": "VerifiablePresentationRequest", "query": [ { "type": "QueryByExample", "credentialQuery": { "reason": "Sign in", "example": { "@context": [ "https:\/\/www.w3.org\/2018\/credentials\/v1" ], "type": "VerifiableCredential" } } } ], "challenge": "4f34494e-43d4-4e08-8b72-d634650daf44", "domain": "demo.spruceid.com" }"#;
+
+// TODO: add get_app() function
