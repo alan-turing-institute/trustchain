1f18e7dac34487d2b22a58a69ef332ec99ec8337
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-04-28 10:30:49 +0100

    Use different issuer

--- trustchain-http/src/lib.rs
+++ trustchain-http/src/lib.rs
@@ -8,7 +8,7 @@ pub mod vc;
 pub mod verifier;
 
 /// Issuer DID
-pub const ISSUER_DID: &str = "did:ion:test:EiBYdto2LQd_uAj_EXEoxP_KbLmZzwe1E-vXp8ZsMv1Gpg";
+pub const ISSUER_DID: &str = "did:ion:test:EiBcLZcELCKKtmun_CUImSlb2wcxK5eM8YXSq3MrqNe5wA";
 
 /// Example VP request used by demo.spruceid.com
 pub const EXAMPLE_VP_REQUEST: &str = r#"{ "type": "VerifiablePresentationRequest", "query": [ { "type": "QueryByExample", "credentialQuery": { "reason": "Sign in", "example": { "@context": [ "https:\/\/www.w3.org\/2018\/credentials\/v1" ], "type": "VerifiableCredential" } } } ], "challenge": "4f34494e-43d4-4e08-8b72-d634650daf44", "domain": "demo.spruceid.com" }"#;
