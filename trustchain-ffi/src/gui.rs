20c60154758d58df32aa8dc7efcac3754f747ad4
Ed Chapman - Turing <edchapmanelc@gmail.com>
2023-05-02 15:28:03 +0100

    Custom error struct pattern

--- trustchain-ffi/src/gui.rs
+++ trustchain-ffi/src/gui.rs
@@ -1,12 +1,10 @@
 use anyhow::anyhow;
 use did_ion::sidetree::DocumentState;
-use serde_json::to_string_pretty;
 use ssi::did_resolve::ResolutionResult;
 use thiserror::Error;
 use trustchain_api::{api::TrustchainDIDAPI, TrustchainAPI};
-use trustchain_core::chain::DIDChain;
-use trustchain_core::verifier::Verifier;
-use trustchain_ion::{get_ion_resolver, verifier::IONVerifier};
+use trustchain_core::resolver::ResolverError;
+use trustchain_core::verifier::VerifierError;
 
 // TODO: implement the below functions that will be used as FFI on desktop GUI. Aim to implement the
 // functions to that they each call a TrustchainCLI method.
@@ -18,8 +16,16 @@ use trustchain_ion::{get_ion_resolver, verifier::IONVerifier};
 enum FFIGUIError {
     #[error("JSON Deserialisation Error: {0}.")]
     FailedToDeserialise(serde_json::Error),
-    #[error("Failed to deserialise: {1} \n Info: {0}")]
+    #[error("JSON Deserialisation Error: {1} \n Info: {0}")]
     FailedToDeserialiseVerbose(String, serde_json::Error),
+    #[error("DID Create Error: {0}.")]
+    FailedToCreateDID(Box<dyn std::error::Error>),
+    #[error("dDID Attest Error: {0}.")]
+    FailedToAttestdDID(Box<dyn std::error::Error>),
+    #[error("DID Resolve Error: {0}.")]
+    FailedToResolveDID(ResolverError),
+    #[error("DID Verify Error: {0}.")]
+    FailedToVerifyDID(VerifierError),
 }
 /// Creates a controlled DID from a passed document state, writing the associated create operation to file in the operations path.
 pub fn create(doc_state: Option<String>, verbose: bool) -> anyhow::Result<()> {
@@ -27,19 +33,12 @@ pub fn create(doc_state: Option<String>, verbose: bool) -> anyhow::Result<()> {
     if let Some(doc_string) = doc_state {
         match serde_json::from_str(&doc_string) {
             Ok(doc) => document_state = Some(doc),
-            // Err(err) => return Err(FFIGUIError::FailedToDeserialise(err).into()),
-            Err(err) => {
-                return Err(anyhow!(
-                    "{}",
-                    FFIGUIError::FailedToDeserialise(err).to_string()
-                ))
-            }
+            Err(err) => return Err(anyhow!("{}",FFIGUIError::FailedToDeserialise(err))),
         }
-        // document_state = Some(serde_json::from_str(&doc_string).unwrap())
     }
     match TrustchainAPI::create(document_state, verbose) {
         Ok(_) => Ok(()),
-        Err(err) => Err(anyhow!("{err}")),
+        Err(err) => Err(anyhow!("{}",FFIGUIError::FailedToCreateDID(err))),
     }
 }
 
@@ -47,31 +46,38 @@ pub fn create(doc_state: Option<String>, verbose: bool) -> anyhow::Result<()> {
 pub fn attest(did: String, controlled_did: String, verbose: bool) -> anyhow::Result<()> {
     match TrustchainAPI::attest(&did, &controlled_did, verbose) {
         Ok(_) => Ok(()),
-        Err(err) => Err(anyhow!("{err}")),
+        Err(err) => Err(anyhow!("{}",FFIGUIError::FailedToAttestdDID(err))),
     }
 }
+
 /// Resolves a given DID using a resolver available at localhost:3000
 pub fn resolve(did: String) -> anyhow::Result<String> {
-    let (res_meta, doc, doc_meta) = TrustchainAPI::resolve(&did, "http://localhost:3000/".into())?;
-    // TODO: refactor conversion into trustchain-core resolve module
-    Ok(serde_json::to_string_pretty(&ResolutionResult {
-        context: Some(serde_json::Value::String(
-            "https://w3id.org/did-resolution/v1".to_string(),
-        )),
-        did_document: doc,
-        did_resolution_metadata: Some(res_meta),
-        did_document_metadata: doc_meta,
-        property_set: None,
-    })?)
+    // let (res_meta, doc, doc_meta) = TrustchainAPI::resolve(&did, "http://localhost:3000/".into())?;
+    match TrustchainAPI::resolve(&did, "http://localhost:3000/".into()) {
+        Ok((res_meta, doc, doc_meta)) =>
+            Ok(serde_json::to_string_pretty(
+                // TODO: refactor conversion into trustchain-core resolve module
+                &ResolutionResult {
+                    context: Some(serde_json::Value::String(
+                        "https://w3id.org/did-resolution/v1".to_string(),
+                    )),
+                    did_document: doc,
+                    did_resolution_metadata: Some(res_meta),
+                    did_document_metadata: doc_meta,
+                    property_set: None,
+                }
+            ).expect("Serialise implimented for ResolutionResult struct")),
+        Err(err) => Err(anyhow!("{}",FFIGUIError::FailedToResolveDID(err)))
+    }
 }
 
 /// TODO: the below have no CLI implementation currently but are planned
 /// Verifies a given DID using a resolver available at localhost:3000, returning a result.
-pub fn verify(did: String, verbose: bool) -> anyhow::Result<String> {
+pub fn verify(did: String) -> anyhow::Result<String> {
     match TrustchainAPI::verify(&did) {
         Ok(did_chain) => Ok(serde_json::to_string_pretty(&did_chain)
-            .expect("Serialize implimented for DIDChain struct")),
-        Err(err) => Err(anyhow!("{err}")),
+            .expect("Serialise implimented for DIDChain struct")),
+        Err(err) => Err(anyhow!("{}",FFIGUIError::FailedToVerifyDID(err))),
     }
 }
 /// Generates an update operation and writes to operations path.
