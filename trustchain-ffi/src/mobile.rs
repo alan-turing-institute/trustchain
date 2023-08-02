394fbb0b004609fba7d84ab6dfedb285fe02f4d4
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-08-02 17:31:46 +0100

    Add FFI mobile error

--- trustchain-ffi/src/mobile.rs
+++ trustchain-ffi/src/mobile.rs
@@ -1,37 +1,62 @@
 // TODO: add module doc comments for mobile FFI
 use crate::config::FFIConfig;
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use ssi::{ldp::now_ms, vc::Credential};
+use thiserror::Error;
 use tokio::runtime::Runtime;
 use trustchain_api::{
     api::{TrustchainDIDAPI, TrustchainVCAPI},
     TrustchainAPI,
 };
+use trustchain_core::{resolver::ResolverError, vc::CredentialError, verifier::VerifierError};
 use trustchain_ion::{get_ion_resolver, verifier::IONVerifier};
+
+/// A speicfic error for FFI mobile making handling easier.
+#[derive(Error, Debug)]
+enum FFIMobileError {
+    /// Failed to deserialize JSON.
+    #[error("JSON Deserialization Error: {0}.")]
+    FailedToDeserialize(serde_json::Error),
+    /// Failed to serialize.
+    #[error("JSON Serialization Error: {0}.")]
+    FailedToSerialize(serde_json::Error),
+    #[error("Missing config error: {0}")]
+    NoConfig(anyhow::Error),
+    #[error("DID Resolve Error: {0}.")]
+    FailedToResolveDID(ResolverError),
+    #[error("DID Verify Error: {0}.")]
+    FailedToVerifyDID(VerifierError),
+    #[error("Failed to verify credential error: {0}.")]
+    FailedToVerifyCredential(CredentialError),
+}
 
 /// Example greet function.
 pub fn greet() -> String {
     format!("Hello from Rust at time: {}", now_ms())
 }
 
-/// Resolves a given DID document.
+/// Resolves a given DID document returning the serialized DID document as a JSON string.
 pub fn did_resolve(did: String, opts: String) -> Result<String> {
-    let mobile_opts: FFIConfig = serde_json::from_str(&opts)?;
-    let endpoint_opts = mobile_opts.endpoint()?;
+    let mobile_opts: FFIConfig =
+        serde_json::from_str(&opts).map_err(FFIMobileError::FailedToDeserialize)?;
+    let endpoint_opts = mobile_opts.endpoint().map_err(FFIMobileError::NoConfig)?;
     let resolver = get_ion_resolver(&endpoint_opts.ion_endpoint().to_address());
     let rt = Runtime::new().unwrap();
     rt.block_on(async {
-        TrustchainAPI::resolve(&did, &resolver)
+        Ok(TrustchainAPI::resolve(&did, &resolver)
             .await
-            .map_err(|e| anyhow!(e))
-            .and_then(|(_, doc, _)| serde_json::to_string_pretty(&doc).map_err(|e| anyhow!(e)))
+            .map_err(FFIMobileError::FailedToResolveDID)
+            .and_then(|(_, doc, _)| {
+                serde_json::to_string_pretty(&doc).map_err(FFIMobileError::FailedToSerialize)
+            })?)
     })
 }
-/// Verifies a given DID.
-pub fn did_verify(did: String, opts: String) -> Result<()> {
-    let mobile_opts: FFIConfig = serde_json::from_str(&opts)?;
-    let endpoint_opts = mobile_opts.endpoint()?;
-    let trustchain_opts = mobile_opts.trustchain()?;
+/// Verifies a given DID returning the serialized DIDChain as a JSON string.
+pub fn did_verify(did: String, opts: String) -> Result<String> {
+    let mobile_opts: FFIConfig =
+        serde_json::from_str(&opts).map_err(FFIMobileError::FailedToDeserialize)?;
+    let endpoint_opts = mobile_opts.endpoint().map_err(FFIMobileError::NoConfig)?;
+    let trustchain_opts = mobile_opts.trustchain().map_err(FFIMobileError::NoConfig)?;
     let root_event_time = trustchain_opts.root_event_time;
     let rt = Runtime::new().unwrap();
     rt.block_on(async {
@@ -39,14 +64,16 @@ pub fn did_verify(did: String, opts: String) -> Result<()> {
             get_ion_resolver(&endpoint_opts.ion_endpoint().to_address()),
             endpoint_opts.trustchain_endpoint()?.to_address(),
         );
-        TrustchainAPI::verify(&did, root_event_time, &verifier)
+        Ok(TrustchainAPI::verify(&did, root_event_time, &verifier)
             .await
-            .map_err(|err| anyhow!(err.to_string()))?;
-        Ok(())
+            .map_err(FFIMobileError::FailedToVerifyDID)
+            .and_then(|did_chain| {
+                serde_json::to_string_pretty(&did_chain).map_err(FFIMobileError::FailedToSerialize)
+            })?)
     })
 }
 
-/// Verifies a verifiable credential.
+/// Verifies a verifiable credential returning the serialized DIDChain as a JSON string.
 pub fn vc_verify_credential(credential: String, opts: String) -> Result<String> {
     let mobile_opts: FFIConfig = serde_json::from_str(&opts)?;
     let endpoint_opts = mobile_opts.endpoint()?;
@@ -67,10 +94,15 @@ pub fn vc_verify_credential(credential: String, opts: String) -> Result<String> 
         // https://docs.rs/ssi/0.4.0/src/ssi/vc.rs.html#1973-1975 (created time checked here)
         //
         // TODO: try setting time as now from emulator to check time used from now_ms() call
-        TrustchainAPI::verify_credential(&credential, ldp_opts, root_event_time, &verifier)
-            .await
-            .map_err(|err| anyhow!(err.to_string()))?;
-        Ok("Ok".to_string())
+        Ok(
+            TrustchainAPI::verify_credential(&credential, ldp_opts, root_event_time, &verifier)
+                .await
+                .map_err(FFIMobileError::FailedToVerifyCredential)
+                .and_then(|did_chain| {
+                    serde_json::to_string_pretty(&did_chain)
+                        .map_err(FFIMobileError::FailedToSerialize)
+                })?,
+        )
     })
 }
 
