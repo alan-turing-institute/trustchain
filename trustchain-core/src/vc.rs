fc52bf1b67f3c441c690693cd7cc2c90ab300b07
Ed Chapman - Turing <edchapmanelc@gmail.com>
2023-09-08 17:01:12 +0100

    completed partial implementation of flatten() for Credential

--- trustchain-core/src/vc.rs
+++ trustchain-core/src/vc.rs
@@ -8,7 +8,9 @@ use ps_sig::rsssig::RSignature;
 use serde_json::{Map, Value};
 use ssi::{
     one_or_many::OneOrMany,
-    vc::{Context, Contexts, Credential, CredentialSubject, Proof, VerificationResult, URI},
+    vc::{
+        Context, Contexts, Credential, CredentialSubject, Issuer, Proof, VerificationResult, URI,
+    },
 };
 use thiserror::Error;
 
@@ -39,6 +41,8 @@ pub trait CanonicalFlatten {
     fn flatten(&self) -> Vec<String>;
 }
 
+/// Convert an un-ordered HashMap over json Value objects into an ordered json Map (which is used
+/// under the hood within json Value objects)
 fn convert_map(map: &HashMap<String, Value>) -> Map<String, Value> {
     // json Value enum varients are all 'ordered' already, so only the top level HashMap must be
     // sorted (the serde_json `Map` implementation uses either BTreeMap or indexmap::IndexMap
@@ -46,6 +50,8 @@ fn convert_map(map: &HashMap<String, Value>) -> Map<String, Value> {
     Map::from_iter(map.clone().into_iter())
 }
 
+// Flatten a json Map of json Values with a 1-to-1 algorithm (ensuring two distinct map cannot
+// produce the same flattened result)
 fn flatten_map(map: &Map<String, Value>) -> Vec<String> {
     let mut res = Vec::new();
     for (k, v) in map {
@@ -55,12 +61,24 @@ fn flatten_map(map: &Map<String, Value>) -> Vec<String> {
             Value::Number(_) => res.push(k.to_owned() + ":" + &v.flatten().first().unwrap()),
             Value::String(_) => res.push(k.to_owned() + ":" + &v.flatten().first().unwrap()),
             Value::Array(_) => {
-                res.push(k.to_owned() + ":[");
-                res.append(&mut v.flatten());
+                // res.push(k.to_owned() + ":[");
+                res.append(
+                    &mut v
+                        .flatten()
+                        .iter()
+                        .map(|s| k.to_owned() + ":[" + s)
+                        .collect(),
+                );
             }
             Value::Object(_) => {
-                res.push(k.to_owned() + ":{");
-                res.append(&mut v.flatten());
+                // res.push(k.to_owned() + ":{");
+                res.append(
+                    &mut v
+                        .flatten()
+                        .iter()
+                        .map(|s| k.to_owned() + ":{" + s)
+                        .collect(),
+                );
             }
         }
     }
@@ -93,101 +111,203 @@ impl CanonicalFlatten for Credential {
     fn flatten(&self) -> Vec<String> {
         let mut res = Vec::new();
 
-        // pub fn convert_map(map: HashMap<String, Value>) -> BTreeMap<String, Value> {
-        //     let btreemap = BTreeMap::from_iter(map.into_iter());
-        //     btreemap
-        // }
-
-        fn handle_context(ctx: &Context) -> Vec<String> {
-            match ctx {
-                Context::URI(uri) => vec![handle_uri(uri)],
-                Context::Object(map) => flatten_map(&convert_map(map)),
+        // flatten only credential_subject
+        match &self.credential_subject {
+            OneOrMany::One(cs) => {
+                // res.push("credentialSubject:{".to_string());
+                res.append(&mut handle_credential_subject(cs));
+            }
+            OneOrMany::Many(_) => {
+                panic!("TODO find *unique* flat representation for multiple subjects");
+                // res.push("credentialSubject:[".to_string());
+                // res.append(cs_vec.iter().fold(&mut Vec::new(), |acc, cs| {
+                //     acc.append(&mut handle_credential_subject(cs));
+                //     acc
+                // }));
             }
         }
 
-        fn handle_uri(uri: &URI) -> String {
-            match uri {
-                URI::String(s) => s.to_string(),
-            }
-        }
-
+        // helper functions
         fn handle_credential_subject(cs: &CredentialSubject) -> Vec<String> {
             let mut res = Vec::new();
             if let Some(uri) = &cs.id {
                 res.push("id:".to_string() + &handle_uri(uri));
             }
             if let Some(map) = &cs.property_set {
-                res.push("propertySet:{".to_string());
+                // res.push("propertySet:{".to_string());
                 res.append(&mut flatten_map(&convert_map(map)));
             }
             res
         }
+        fn handle_uri(uri: &URI) -> String {
+            match uri {
+                URI::String(s) => s.to_string(),
+            }
+        }
 
+        // Build a metadata map containing all other fields, to then be compressed into one String.
+        // This simplifies the implementation, whilst only allowing the holder to redact fields from
+        // the credentialSubject field.
+        let mut metadata = BTreeMap::new();
+
+        // The metadata map must be strictly ordered (along with the flattened credentialSubject
+        // fields). All fields in the `Credential` struct that contain un-ordered data structures must
+        // be converted to ordered structures before serializing into metadata map values.
+        // Fields with unordered data structures are:
         // context
-        res.push("context:{".to_string());
-        match &self.context {
-            Contexts::One(ctx) => res.append(&mut handle_context(&ctx)),
-            Contexts::Many(ctx_vec) => {
-                res = ctx_vec.iter().fold(res, |mut acc, ctx| {
-                    acc.append(&mut handle_context(ctx));
-                    acc
-                });
-            }
-        }
-
-        // id
-        if let Some(uri) = &self.id {
-            res.push("id".to_string() + ":" + &handle_uri(uri));
-        }
-
-        // type_
-        match &self.type_ {
-            OneOrMany::One(t) => res.push("type".to_string() + ":" + t),
-            OneOrMany::Many(t_vec) => {
-                res.push("type:[".to_string());
-                for t in t_vec {
-                    res.push(t.to_string());
-                }
-            }
-        }
-
         // credential_subject
-        res.push("credentialSubject:{".to_string());
-        match &self.credential_subject {
-            OneOrMany::One(cs) => {
-                res.push("credentialSubject:{".to_string());
-                res.append(&mut handle_credential_subject(cs));
-            }
-            OneOrMany::Many(cs_vec) => {
-                res.push("credentialSubject:[".to_string());
-                res.append(cs_vec.iter().fold(&mut Vec::new(), |acc, cs| {
-                    acc.append(&mut handle_credential_subject(cs));
-                    acc
-                }));
-            }
-        }
-
         // issuer
-        if let Some(issuer) = &self.issuer {
-            match issuer {
-                ssi::vc::Issuer::URI(uri) => {
-                    res.push("issuer".to_string() + ":" + &handle_uri(uri));
-                }
-                ssi::vc::Issuer::Object(obj) => {
-                    res.push("issuer:{".to_string());
-                    res.push("id:".to_string() + &handle_uri(&obj.id));
-                    if let Some(map) = &obj.property_set {
-                        res.push("propertySet:{".to_string());
-                        res.append(&mut flatten_map(&convert_map(&map)));
+        // proof (this field is ignored by `flatten()` because the RSS proofs handled seperately)
+        // credential_status
+        // terms_of_use
+        // evidence
+        // credential_schema
+        // refresh_service
+        // property_set
+
+        // context _________________________________
+        let context_string = match &self.context {
+            Contexts::One(ctx) => match ctx {
+                Context::URI(_) => serde_json::to_string(ctx).unwrap(),
+                Context::Object(map) => serde_json::to_string(&convert_map(map)).unwrap(),
+            },
+            Contexts::Many(ctx_vec) => serde_json::to_string(
+                &ctx_vec
+                    .iter()
+                    .map(|ctx| {
+                        if let Context::Object(map) = ctx {
+                            serde_json::to_string(&convert_map(map)).unwrap()
+                        } else {
+                            serde_json::to_string(ctx).unwrap()
+                        }
+                    })
+                    .collect::<Vec<String>>(),
+            )
+            .unwrap(),
+        };
+        metadata.insert("@context", context_string);
+
+        // id _________________________________
+        metadata.insert("id", serde_json::to_string(&self.id).unwrap());
+
+        // type _________________________________
+        metadata.insert("type", serde_json::to_string(&self.type_).unwrap());
+
+        // issuer _________________________________
+        if let Some(v) = &self.issuer {
+            let issuer_string = match v {
+                Issuer::URI(_) => serde_json::to_string(v).unwrap(),
+                Issuer::Object(obj) => {
+                    if let Some(prop_set) = &obj.property_set {
+                        serde_json::to_string(&obj.id).unwrap()
+                            + &serde_json::to_string(&convert_map(&prop_set)).unwrap()
+                    } else {
+                        serde_json::to_string(v).unwrap()
                     }
                 }
+            };
+            metadata.insert("issuer", issuer_string);
+        }
+
+        // issuance_date _________________________________
+        if let Some(v) = &self.issuance_date {
+            metadata.insert("issuanceDate", serde_json::to_string(v).unwrap());
+        }
+
+        // expiration_date _________________________________
+        if let Some(v) = &self.expiration_date {
+            metadata.insert("expirationDate", serde_json::to_string(v).unwrap());
+        }
+
+        // credential_status _________________________________
+        if let Some(v) = &self.credential_status {
+            let c_s_string = if let Some(prop_set) = &v.property_set {
+                serde_json::to_string(&v.id).unwrap()
+                    + &serde_json::to_string(&v.type_).unwrap()
+                    + &serde_json::to_string(&convert_map(&prop_set)).unwrap()
+            } else {
+                serde_json::to_string(v).unwrap()
+            };
+            metadata.insert("credentialStatus", c_s_string);
+        }
+
+        // unimplemented _________________________________
+        if let Some(_) = &self.terms_of_use {
+            todo!()
+        }
+        if let Some(_) = &self.evidence {
+            todo!()
+        }
+        if let Some(_) = &self.credential_schema {
+            todo!()
+        }
+        if let Some(_) = &self.refresh_service {
+            todo!()
+        }
+        if let Some(prop_set) = &self.property_set {
+            if prop_set.len() != 0 {
+                todo!()
             }
         }
 
-        // issuance_date
-        if let Some(date) = &self.issuance_date {
-            res.push("issuanceDate:".to_string() + &serde_json::to_string(date).unwrap());
-        }
+        res.push("metadata:".to_string() + &serde_json::to_string(&metadata).unwrap());
+
+        // fn handle_context(ctx: &Context) -> Vec<String> {
+        //     match ctx {
+        //         Context::URI(uri) => vec![handle_uri(uri)],
+        //         Context::Object(map) => flatten_map(&convert_map(map)),
+        //     }
+        // }
+
+        // // context
+        // res.push("context:{".to_string());
+        // match &self.context {
+        //     Contexts::One(ctx) => res.append(&mut handle_context(&ctx)),
+        //     Contexts::Many(ctx_vec) => {
+        //         res = ctx_vec.iter().fold(res, |mut acc, ctx| {
+        //             acc.append(&mut handle_context(ctx));
+        //             acc
+        //         });
+        //     }
+        // }
+
+        // // id
+        // if let Some(uri) = &self.id {
+        //     res.push("id".to_string() + ":" + &handle_uri(uri));
+        // }
+
+        // // type_
+        // match &self.type_ {
+        //     OneOrMany::One(t) => res.push("type".to_string() + ":" + t),
+        //     OneOrMany::Many(t_vec) => {
+        //         res.push("type:[".to_string());
+        //         for t in t_vec {
+        //             res.push(t.to_string());
+        //         }
+        //     }
+        // }
+
+        // // issuer
+        // if let Some(issuer) = &self.issuer {
+        //     match issuer {
+        //         ssi::vc::Issuer::URI(uri) => {
+        //             res.push("issuer".to_string() + ":" + &handle_uri(uri));
+        //         }
+        //         ssi::vc::Issuer::Object(obj) => {
+        //             res.push("issuer:{".to_string());
+        //             res.push("id:".to_string() + &handle_uri(&obj.id));
+        //             if let Some(map) = &obj.property_set {
+        //                 res.push("propertySet:{".to_string());
+        //                 res.append(&mut flatten_map(&convert_map(&map)));
+        //             }
+        //         }
+        //     }
+        // }
+
+        // // issuance_date
+        // if let Some(date) = &self.issuance_date {
+        //     res.push("issuanceDate:".to_string() + serde_json::to_string(date).unwrap());
+        // }
 
         res
     }
@@ -227,6 +347,7 @@ mod tests {
             "0":"did:example:testdidsuffix"
           }
         ],
+        "issuer": "did:ion:test:EiAtHHKFJWAk5AsM3tgCut3OiBY4ekHTf66AAjoysXL65Q",
         "id": "did:ion:test_id_field",
         "type": ["VerifiableCredential"],
         "credentialSubject": {
@@ -235,23 +356,26 @@ mod tests {
             "degree": {
               "type": "BachelorDegree",
               "name": "Bachelor of Science and Arts",
-              "college": "College of Engineering"
+              "college": "College of Engineering",
+              "nested" : {
+                "key" : "value"
+              },
+              "testArray": [
+                "element",
+                {"objectInArray": {
+                    "two":"val",
+                    "one":"val"
+                }}
+              ]
             }
         }
       }
       "##;
 
-    //   "credentialSchema": {
-    //     "id": "did:example:cdf:35LB7w9ueWbagPL94T9bMLtyXDj9pX5o",
-    //     "type": "did:example:schema:22KpkXgecryx9k7N6XN1QoN3gXwBkSU8SfyyYQG"
-    //   },
-    //   "issuer": "did:ion:test:EiAtHHKFJWAk5AsM3tgCut3OiBY4ekHTf66AAjoysXL65Q",
-    //   "image": "some_base64_representation",
-
     #[test]
     fn deserialize() {
-        // let vc: OCredential = serde_json::from_str(TEST_UNSIGNED_VC).unwrap();
-        // println!("{}", serde_json::to_string_pretty(&vc.flatten()).unwrap());
+        let vc: Credential = serde_json::from_str(TEST_UNSIGNED_VC).unwrap();
+        println!("{}", serde_json::to_string_pretty(&vc.flatten()).unwrap());
     }
 
     #[test]
