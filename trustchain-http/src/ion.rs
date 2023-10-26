1e40f563d77a1313662a4aa5abdc6aee76f69166
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-10-26 17:52:52 +0100

    Modify post operation to get address from config, add test

--- trustchain-http/src/ion.rs
+++ trustchain-http/src/ion.rs
@@ -11,13 +11,17 @@ use std::sync::Arc;
 /// Receives ION operation POST request and forwards to local ION node.
 pub async fn post_operation(
     Json(operation): Json<Value>,
-    _app_state: Arc<AppState>,
+    app_state: Arc<AppState>,
 ) -> impl IntoResponse {
     info!("Received ION operation: {}", operation);
     let client = reqwest::Client::new();
+    let address = format!(
+        "http://{}:{}/operations",
+        app_state.config.ion_host, app_state.config.ion_port
+    );
     client
         // TODO: Add config for ION URL
-        .post("http://localhost:3000/operations")
+        .post(address)
         .json(&operation)
         .send()
         .await
@@ -34,9 +38,61 @@ pub async fn post_operation(
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
-    // TODO: add test by setting up:
-    //   - a mock server as the ION server
-    //   - a test server with the `/operations` route
-    // And then sending a POST reqwest and testing response
+    use super::*;
+    use crate::{config::HTTPConfig, server::TrustchainRouter};
+    use axum::{response::Html, routing::post, Router};
+    use hyper::{Server, StatusCode};
+    use std::{collections::HashMap, net::TcpListener};
+    use tower::make::Shared;
+
+    async fn mock_post_operation_handler(Json(json): Json<Value>) -> impl IntoResponse {
+        Html(format!("Response: {}", json))
+    }
+
+    #[tokio::test]
+    async fn test_post_operation() {
+        let listener = TcpListener::bind("127.0.0.1:0").expect("Could not bind ephemeral socket");
+        let trustchain_addr = listener.local_addr().unwrap();
+        let trustchain_port = trustchain_addr.port();
+        let ion_listener =
+            TcpListener::bind("127.0.0.1:0").expect("Could not bind ephemeral socket");
+        let ion_addr = ion_listener.local_addr().unwrap();
+        let ion_port = ion_addr.port();
+        let http_config = HTTPConfig {
+            port: trustchain_port,
+            ion_port,
+            ..Default::default()
+        };
+        assert_eq!(
+            http_config.host.to_string(),
+            trustchain_addr.ip().to_string()
+        );
+
+        // Run server
+        tokio::spawn(async move {
+            let trustchain_server = Server::from_tcp(listener).unwrap().serve(Shared::new(
+                TrustchainRouter::from(http_config).into_router(),
+            ));
+            trustchain_server.await.expect("server error");
+        });
+
+        // Run mock ION server
+        tokio::spawn(async move {
+            let ion_server = Server::from_tcp(ion_listener).unwrap().serve(Shared::new(
+                Router::new().route("/operations", post(mock_post_operation_handler)),
+            ));
+            ion_server.await.expect("server error");
+        });
+
+        // Send POST request to server
+        let client = reqwest::Client::new();
+        let addr = format!("http://127.0.0.1:{trustchain_port}/operations");
+        let map: HashMap<String, String> = serde_json::from_str(r#"{"key": "value"}"#).unwrap();
+        let response = client.post(&addr).json(&map).send().await.unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        assert_eq!(
+            r#"Response: {"key":"value"}"#,
+            response.text().await.unwrap()
+        );
+    }
 }
