d93b064e22bacdd9b92b8ea065fcfda63839a804
pwochner <pwochner@turing.ac.uk>
2023-11-10 17:45:53 +0100

    Make methods to get signing keys public.

--- trustchain-ion/src/attestor.rs
+++ trustchain-ion/src/attestor.rs
@@ -34,12 +34,14 @@ impl IONAttestor {
         }
     }
     /// Gets the signing keys of the attestor.
-    fn signing_keys(&self) -> Result<OneOrMany<JWK>, KeyManagerError> {
+    /// TODO: made public to use in challenge-response. Consider refactoring key manager.
+    pub fn signing_keys(&self) -> Result<OneOrMany<JWK>, KeyManagerError> {
         self.read_signing_keys(self.did_suffix())
     }
 
     /// Gets the signing key with ID `key_id` of the attestor.
-    fn signing_key(&self, key_id: Option<&str>) -> Result<JWK, KeyManagerError> {
+    /// TODO: made public to use in challenge-response. Consider refactoring key manager.
+    pub fn signing_key(&self, key_id: Option<&str>) -> Result<JWK, KeyManagerError> {
         let keys = self.signing_keys()?;
         // If no key_id is given, return the first available key.
         if let Some(key_id) = key_id {
