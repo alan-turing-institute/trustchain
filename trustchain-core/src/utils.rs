fd38d79be1c995735844e2a2ff8c969f8b107776
Sam Greenbury <sgreenbury@turing.ac.uk>
2024-09-06 15:09:45 +0100

    Remove obsolete function

--- trustchain-core/src/utils.rs
+++ trustchain-core/src/utils.rs
@@ -301,18 +301,6 @@ pub fn generate_key() -> JWK {
     JWK::generate_secp256k1().expect("Could not generate key.")
 }
 
-#[allow(dead_code)]
-pub fn set_panic_hook() {
-    // When the `console_error_panic_hook` feature is enabled, we can call the
-    // `set_panic_hook` function at least once during initialization, and then
-    // we will get better error messages if our code ever panics.
-    //
-    // For more details see
-    // <https://github.com/rustwasm/console_error_panic_hook#readme>
-    #[cfg(feature = "console_error_panic_hook")]
-    console_error_panic_hook::set_once();
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
