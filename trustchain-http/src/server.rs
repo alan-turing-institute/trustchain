aa58312a8cca3b348264e2c85f8fc59442383831
Sam Greenbury <sgreenbury@turing.ac.uk>
2024-09-07 11:01:54 +0100

    Fix warnings

--- trustchain-http/src/server.rs
+++ trustchain-http/src/server.rs
@@ -1,13 +1,10 @@
-use crate::attestation_utils::CustomResponse;
 use crate::attestor;
 use crate::config::http_config;
 use crate::middleware::validate_did;
 use crate::{
     config::HTTPConfig, issuer, resolver, root, state::AppState, static_handlers, verifier,
 };
-use axum::extract::Path;
 use axum::routing::{post, IntoMakeService};
-use axum::Json;
 use axum::{middleware, routing::get, Router};
 use axum_server::tls_rustls::RustlsConfig;
 use hyper::server::conn::AddrIncoming;
