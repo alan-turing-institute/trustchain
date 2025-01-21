eff6a18ce2e6d7031393298a7e88fe6f34381b11
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-01-21 22:05:30 +0000

    Add features for HTTP, ION and FFI

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@
 
 pub use trustchain_api as api;
 pub use trustchain_core as core;
+#[cfg(feature = "ffi")]
 pub use trustchain_ffi as ffi;
+#[cfg(feature = "http")]
 pub use trustchain_http as http;
+#[cfg(feature = "ion")]
 pub use trustchain_ion as ion;
